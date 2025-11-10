2cf547874aa18d3c0a39937797138318efb6627d
chrispo <haasc865@gmail.com>
2025-11-10 20:17:54 +0000

    Format Fixes

--- dfaRunner/main.py
+++ dfaRunner/main.py
@@ -4,30 +4,38 @@ import os
 from pathlib import Path
 import argparse
 
+
 class StateError(Exception):
     pass
+
+
 class LanguageError(Exception):
     pass
+
+
 class DFAParseError(Exception):
     pass
+
 
 class State:
     def __init__(self, name: str):
         if not isinstance(name, str):
             raise TypeError("Not type str")
         self.stateName = name
+
     def __eq__(self, other):
         return isinstance(other, State) and other.stateName == self.stateName
-    
+
+
 class StateTransition:
     def __init__(self, stateStart: State, stateEnd: State, transitionChar: str):
-      if not isinstance(stateStart, State) or not isinstance(stateEnd, State):
-         raise TypeError(f"Not type State")
-      if not isinstance(transitionChar, str):
-         raise TypeError("Not type str")
-      self.stateStart = stateStart
-      self.stateEnd = stateEnd
-      self.transitionChar = transitionChar
+        if not isinstance(stateStart, State) or not isinstance(stateEnd, State):
+            raise TypeError(f"Not type State")
+        if not isinstance(transitionChar, str):
+            raise TypeError("Not type str")
+        self.stateStart = stateStart
+        self.stateEnd = stateEnd
+        self.transitionChar = transitionChar
 
     @classmethod
     def from_str(cls, string: str):
@@ -40,13 +48,22 @@ class StateTransition:
         stateEnd = State(string.split(")->")[1])
         transitionChar = string.split("-(")[1].split(")->")[0]
         return cls(stateStart, stateEnd, transitionChar)
+
     def toStr(self):
         return f"{self.stateStart.stateName}-({self.transitionChar})->{self.stateEnd.stateName}"
-      
-      
+
+
 class DefiniteFiniteAutomata:
-    
-    def __init__(self, states: list, acceptingStates: list, startState: State, transitions: list, language: list, verbose=False):
+
+    def __init__(
+        self,
+        states: list,
+        acceptingStates: list,
+        startState: State,
+        transitions: list,
+        language: list,
+        verbose=False,
+    ):
         self.states = []
         self.accepting = []
         self.startState = None
@@ -73,7 +90,7 @@ class DefiniteFiniteAutomata:
         if startState not in states:
             raise StateError("Starting state not found in states")
         if verbose:
-                print(f"Setting {startState.stateName} to start state")
+            print(f"Setting {startState.stateName} to start state")
         self.startState = startState
 
         for transition in transitions:
@@ -89,33 +106,35 @@ class DefiniteFiniteAutomata:
                 self.transitions.append(transition)
             else:
                 raise StateError("Transition state not found in states")
+
     @classmethod
     def from_toml(cls, file: str, verbose=False):
         dfaToml = ""
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             dfaToml = toml.load(f)
         f.close()
         states = []
         acceptingStates = []
         transitions = []
-        for entry in dfaToml['states']:
+        for entry in dfaToml["states"]:
             states.append(State(entry))
-        for entry in dfaToml['accept']:
+        for entry in dfaToml["accept"]:
             acceptingStates.append(State(entry))
-        startState = State(dfaToml['start'])
-        language = dfaToml['language']
-        for entry in dfaToml['transitions']:
+        startState = State(dfaToml["start"])
+        language = dfaToml["language"]
+        for entry in dfaToml["transitions"]:
             transitions.append(StateTransition.from_str(entry))
 
         # states: list, acceptingStates: list, startState: State, transitions: list, language: list
 
         return cls(states, acceptingStates, startState, transitions, language)
-    
+
     def testWord(self, word: str):
         state = self.startState
         for char in word:
-          state = self.next(state, char)
+            state = self.next(state, char)
         return state in self.accepting
+
     def testWordVerbose(self, word: str):
         state = self.startState
         print(f"{state.stateName} ", end="")
@@ -124,22 +143,26 @@ class DefiniteFiniteAutomata:
             print(f"-> {state.stateName} ", end="")
         print("")
         return state in self.accepting
+
     def next(self, state, char: str):
         for trans in self.transitions:
             if trans.stateStart == state and trans.transitionChar == char:
                 return trans.stateEnd
-        
+
         raise LanguageError(f"{char} not in language")
+
     def getStates(self):
         stateList = []
         for state in self.states:
             stateList.append(str(state))
         return stateList
+
     def getAcceptingStates(self):
         stateList = []
         for state in self.accepting:
             stateList.append(str(state))
         return stateList
+
     def printTransitions(self):
         for transition in self.transitions:
             print(transition.toStr())
@@ -150,13 +173,9 @@ def cli():
         description="Run words through A DFA specified in a given toml file"
     )
     parser.add_argument(
-        "dfa", 
-        help="DFA in toml format (check examples folder for examples)"
+        "dfa", help="DFA in toml format (check examples folder for examples)"
     )
-    parser.add_argument(
-        "word", 
-        help="The word to be tested through the DFA"
-    )
+    parser.add_argument("word", help="The word to be tested through the DFA")
     args = parser.parse_args()
     path = Path(args.dfa)
     if not path.is_absolute():
